ccddef2c6a60785bfcb92230dd62d5c86840a15c
Rishabh Rawat <rishabhrawat1994@gmail.com>
2016-07-23 00:56:44 +0530

    modularised code

--- src/javascript/schematic.js
+++ src/javascript/schematic.js
@@ -2,23 +2,28 @@ var draw;
 var cppfuncs = {};
 var actives;
 
-cppfuncs.createWire = function(x0,y0,x1,y1,width) {
+cppfuncs.createWire = function(x0, y0, x1, y1, width) {
 	'use strict';
-	draw.line(x0,y0,x1,y1).stroke({ width: width, color: 'black' });
+	draw.line(x0, y0, x1, y1).stroke({width: width, color: 'black'});
 };
 
-cppfuncs.createRedActive = function(x0,y0,x1,y1,width) {
+cppfuncs.createRedActive = function(x0, y0, x1, y1, width) {
 	'use strict';
-	actives.line(x0,y0,x1,y1).stroke({ width: width, color: 'red' });
+	actives.line(x0, y0, x1, y1).stroke({width: width, color: 'red'});
 };
-cppfuncs.createBlueActive = function(x0,y0,x1,y1,width) {
+cppfuncs.createBlueActive = function(x0, y0, x1, y1, width) {
 	'use strict';
-	actives.line(x0,y0,x1,y1).stroke({ width: width, color: 'blue' });
+	actives.line(x0, y0, x1, y1).stroke({width: width, color: 'blue'});
 };
 
-cppfuncs.removeActives = function () {
+cppfuncs.removeActives = function() {
 	'use strict';
 	actives.clear();
+};
+
+var createRectChild = function(svgElem, x, y, size_x, size_y, strokeWidth) {
+	svgElem.rect(size_x, size_y).attr({x: x, y: y, fill: '#fff'}).stroke({width: strokeWidth});
+	return svgElem.nested().size(size_x, size_y).attr({x: x, y: y});
 };
 
 createDetailedSchematic = function(div, jsonString) {
@@ -26,12 +31,6 @@ createDetailedSchematic = function(div, jsonString) {
 	var schematic = new Module.schematic();
 	var string = JSON.stringify(jsonString);
 	var data = JSON.parse(schematic.createDetailedJsonSchematicFromJson(string));
-
-	var createRectChild = function(svgElem, x, y, size_x, size_y, strokeWidth) {
-		svgElem.rect(size_x, size_y).attr({x: x, y: y, fill: '#fff'}).stroke({width: strokeWidth});
-		return svgElem.nested().size(size_x, size_y).attr({x: x, y: y});
-	};
-
 
 	draw = SVG(div).size(data.size_x + 6, data.size_y + 6);
 	var tRect = createRectChild(draw, 0, 0, data.size_x, data.size_y, 1);
@@ -56,12 +55,6 @@ createSchematic = function(div, jsonString) {
 	'use strict';
 	var schematic = new Module.schematic();
 	var result = schematic.createJsonSchematicFromJson(JSON.stringify(jsonString))
-	console.log(result);
-	var data = JSON.parse(result);
-	var createRectChild = function(svgElem, x, y, size_x, size_y, strokeWidth) {
-		svgElem.rect(size_x, size_y).attr({x: x, y: y, fill: '#fff'}).stroke({width: strokeWidth});
-		return svgElem.nested().size(size_x, size_y).attr({x: x, y: y});
-	};
 
 	draw = SVG(div).size(data.size_x + 6, data.size_y + 6);
 	var tRect = createRectChild(draw, 0, 0, data.size_x, data.size_y, 1);
@@ -70,11 +63,11 @@ createSchematic = function(div, jsonString) {
 	for (var i = 0; i < data.modules.length; i++) {
 		var mod = data.modules[i];
 		var mRect = createRectChild(tRect, mod.pos_x, mod.pos_y, mod.size_x, mod.size_y, 1);
-		for(var j = 0; j < mod.terminals.length; j++)
+		for (var j = 0; j < mod.terminals.length; j++)
 			draw.circle(10).attr({cx: mod.terminals[j].pos_x, cy: mod.terminals[j].pos_y, color: 'red'});
 	}
 
-	for(var k = 0; k < data.systemTerminals.length; k++)
+	for (var k = 0; k < data.systemTerminals.length; k++)
 		draw.circle(10).attr({cx: data.systemTerminals[k].pos_x, cy: data.systemTerminals[k].pos_y, color: 'red'});
 	schematic.doRouting();
 };
